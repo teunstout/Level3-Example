3e8d94bc05f03ab9a793f33df02d8ce05ba82e63
TeunStout <teunstout@hotmail.com>
2020-03-09 17:20:49 +0100

    2.2 done

--- RemindersApp/app/src/main/java/com/example/remindersapp/Reminder.kt
+++ RemindersApp/app/src/main/java/com/example/remindersapp/Reminder.kt
@@ -1,6 +1,10 @@
 package com.example.remindersapp
 
-data class Reminder(val name: String){
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class Reminder(val name: String) : Parcelable {
     companion object{
         val STANDERTREMINDERS = arrayOf(
             "Koken",
